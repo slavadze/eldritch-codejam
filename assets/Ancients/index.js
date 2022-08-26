a9bb05d90a5e4351db545d100f82fd4c430429b1
slavadze <6354045@mail.ru>
2022-08-26 09:42:17 +0300

    feat: add ancients

--- assets/Ancients/index.js
+++ assets/Ancients/index.js
@@ -1,13 +1,40 @@
-import azathoth from './Azathoth.png';
-import cthulhu from './Cthulthu.png';
-import iogSothoth from './IogSothoth.png';
-import shubNiggurath from './ShubNiggurath.png';
+const ancients = [
+  {
+    title: "azzayhoth",
+    src: "./assets/Ancients/Azathoth.jpg",
+    stage: [
+      [1, 2, 1],
+      [2, 3, 1],
+      [2, 4, 0],
+    ],
+  },
+  {
+    title: "cthulhu",
+    src: "./assets/Ancients/Cthulthu.jpg",
+    stage: [
+      [0, 2, 2],
+      [1, 3, 0],
+      [3, 4, 0],
+    ],
+  },
+  {
+    title: "solthoth",
+    src: "./assets/Ancients/IogSothoth.jpg",
+    stage: [
+      [0, 2, 1],
+      [2, 3, 1],
+      [3, 4, 0],
+    ],
+  },
+  {
+    title: "shubNiggurath",
+    src: "./assets/Ancients/ShubNiggurath.jpg",
+    stage: [
+      [1, 2, 1],
+      [3, 2, 1],
+      [2, 4, 0],
+    ],
+  },
+];
 
-const ancients = {
-  azathoth,
-  cthulhu,
-  iogSothoth,
-  shubNiggurath
-}
-
-export default ancients
+export default ancients;
